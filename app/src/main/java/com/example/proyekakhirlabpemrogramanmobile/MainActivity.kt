de4b9f4ce9142ddcf18783ebabfc5bace87c8116
delon-nazara <delon.gf.nazara@gmail.com>
2024-10-22 20:39:20 +0700

    mengkonfigurasi firebase firestore

--- app/src/main/java/com/example/proyekakhirlabpemrogramanmobile/MainActivity.kt
+++ app/src/main/java/com/example/proyekakhirlabpemrogramanmobile/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.proyekakhirlabpemrogramanmobile
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Inisialisasi firebase firestore
+        val db = Firebase.firestore
+
+        // Membuat data dummy
+        val user = hashMapOf(
+            "name" to "Delon",
+            "nim" to "221401073"
+        )
+
+        // Menambahkan data ke dalam firestore
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d("firestore_log", "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w("firestore_log", "Error adding document", e)
+            }
     }
 }
